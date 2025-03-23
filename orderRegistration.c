478ba48c123d849cce8cc988a614a1ed549b1bdf
João Serafim <victorserafim2003@gmail.com>
2025-03-23 18:50:09 -0300

    added item struct, wich interacts with the order

--- orderRegistration.c
+++ orderRegistration.c
@@ -3,27 +3,77 @@
 #include <string.h>
 
 typedef struct {
-    int unsigned id;
-    char item[50];
+    char code[7];
+    char name[50];
     float value;
-    int unsigned count;
+    int available;
+} Item;
+
+typedef struct {
+    int unsigned id;
+    char itemCode[7];
+    float orderTotal;
+    int unsigned quantity;
 } Order;
 
-int addOrder(int index, Order *ordPointer) {
+int addOrder(int index, Order *ordPointer, Item *itemPointer, long unsigned int itemLen) {
+    char fkItemCode[7];
+    int orderQuantity;
+    int itemFound = 0;
+    int itemIndex;
+    
     ordPointer->id = index;
-    printf("Digite o item do pedido: ");
-    fgets(ordPointer->item , 50, stdin);
+    printf("Type the item code: ");
+    fgets(fkItemCode, 7, stdin);
+    
+    for (int i = 0; i < (itemLen / sizeof(Item)); i++) {
+        if (strcmp(itemPointer[i].code, fkItemCode) == 0) {
+            strcpy(ordPointer->itemCode, fkItemCode);
+            itemFound = 1;
+            itemIndex = i;
+            break;
+        }
+    }
+    
+    if (!itemFound) {
+        printf("Item code is invalid. Try again");
+        return 0;
+    }
+    
+    printf("Type the ordered amount: ");
+    scanf("%d", &orderQuantity);
+    
+    if ((itemPointer[itemIndex].available - orderQuantity) < 0) {
+        printf("There are not enough items in the storage. Please check:\n");
+        printf("%s - %s\n", itemPointer[itemIndex].code, itemPointer[itemIndex].name);
+        printf("Ordered amount: %d\nAvailable amount: %d\n", orderQuantity, itemPointer[itemIndex].available);
+    }
+    
 };
 
 int main()
 {
+    Item item[2];
+    Item *itemPointer = item;
+    
+    strcpy(item[0].code, "ST0001");
+    strcpy(item[0].name, "Blue Shirt");
+    item[0].value = 32.99;
+    item[0].available = 4;
+    
+    strcpy(item[1].code, "ST0002");
+    strcpy(item[1].name, "Yellow Shirt");
+    item[1].value = 29.99;
+    item[1].available = 10;
+    
+    
     Order *order = NULL;
     order = (Order *) malloc(sizeof(Order));
-    Order *p = order;
     int orderCount = 0;
     
-    addOrder(orderCount, &order[orderCount]);
-    printf("Id: %d\nItem: %s\n", order[0].id, order[0].item);
+    addOrder(orderCount, &order[orderCount], itemPointer, sizeof(item));
+    
+    free(order);
     
     return 0;
 }
